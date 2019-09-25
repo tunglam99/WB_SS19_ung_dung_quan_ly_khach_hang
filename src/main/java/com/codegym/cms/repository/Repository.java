e8426fa28c39afdb895239ccfeb1ceeff2fb10ea
Nguyen Tung Lam <lamnthnue.edu.vn@gmail.com>
2019-09-25 18:18:23 +0700

    'ok'

---
+++ src/main/java/com/codegym/cms/repository/Repository.java
@@ -0,0 +1,10 @@
+package com.codegym.cms.repository;
+
+import java.util.List;
+
+public interface Repository<T> {
+    List<T> findAll();
+    T findById(Long id);
+    void save(T model);
+    void remove(Long id);
+}
